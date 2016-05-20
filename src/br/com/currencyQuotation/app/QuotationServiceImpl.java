88c1826d4a7718b81b5f745c6065ebdd10e82b48
riguel figueiro <riguel@MacBook-Pro-de-riguel.local>
2016-05-20 00:20:05 -0300

    refactor in quotation service

--- src/br/com/currencyQuotation/app/QuotationServiceImpl.java
+++ src/br/com/currencyQuotation/app/QuotationServiceImpl.java
@@ -3,6 +3,7 @@ package br.com.currencyQuotation.app;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 import br.com.currencyQuotation.domain.CotacaoEntity;
 import br.com.currencyQuotation.domain.CurrencyQuotationEntity;
@@ -10,42 +11,60 @@ import br.com.currencyQuotation.domain.exception.QuotationException;
 import br.com.currencyQuotation.domain.predicate.CotacaoPredicate;
 import br.com.currencyQuotation.domain.service.QuotationService;
 
-public class QuotationServiceImpl implements QuotationService{
+public class QuotationServiceImpl implements QuotationService {
 
-	CotacaoPredicate predicate = new CotacaoPredicate();
 	private List<CotacaoEntity> cotacoes;
-	
-	public QuotationServiceImpl(List<CotacaoEntity> cotacoes){
+	private CurrencyQuotationEntity currencyQuotationEntity;
+
+	public QuotationServiceImpl(List<CotacaoEntity> cotacoes) {
 		this.cotacoes = cotacoes;
 	}
-	
+
 	@Override
 	public BigDecimal currencyQuotation(String from, String to, Number value, String quotation) {
+
+		currencyQuotationEntity = new CurrencyQuotationEntity(from, to, value, quotation);
+
+		ValidateQuotations();
+
+		List<CotacaoEntity> quotationFinded = CotacaoPredicate.filterCotacao(this.cotacoes,
+				CotacaoPredicate.containsDate(currencyQuotationEntity.getQuotation()));
 		
-		CurrencyQuotationEntity currencyQuotation = new CurrencyQuotationEntity(from, to, value, quotation);
-		
-		CotacaoEntity quotationFrom = predicate.filterCotacao(this.cotacoes, CotacaoPredicate.containsCurrency(currencyQuotation.getFrom())).get(0);
-		CotacaoEntity quotationTo = predicate.filterCotacao(this.cotacoes, CotacaoPredicate.containsCurrency(currencyQuotation.getTo())).get(0);
-		
-		if(quotationFrom == null || quotationTo == null){
+		if ( quotationFinded.size() <= 0) {
+			
+			CotacaoEntity lastQuotation = getLastQuatation(this.cotacoes, currencyQuotationEntity.getQuotation());
+			
+			return lastQuotation.getTaxaCompra().multiply(new BigDecimal(value.toString()));
+		}
+
+		return quotationFinded.get(0).getTaxaCompra().multiply(new BigDecimal(value.toString()));
+	}
+
+	private void ValidateQuotations() {
+		Boolean isValidQuotationFrom = CotacaoPredicate.filterCotacao(this.cotacoes, CotacaoPredicate.containsCurrency(currencyQuotationEntity.getFrom().toUpperCase())).size() > 0;
+		Boolean isValidQuotationTo = CotacaoPredicate.filterCotacao(this.cotacoes,CotacaoPredicate.containsCurrency(currencyQuotationEntity.getTo().toUpperCase())).size() > 0;
+
+		if (!isValidQuotationFrom  || !isValidQuotationTo) {
 			new QuotationException("Invalid quotations from and/or to!");
 		}
+	}
+
+	private CotacaoEntity getLastQuatation(List<CotacaoEntity> cotacoes, LocalDate dateQuotation)
+			throws NoSuchElementException {
+
+		CotacaoEntity lastQuotation = null;
 		
-		if(predicate.filterCotacao(this.cotacoes, CotacaoPredicate.containsDate(currencyQuotation.getQuotation())) == null){
-			CotacaoEntity lastQuotation = getLastQuatation(this.cotacoes, currencyQuotation.getQuotation());
-			
-			if(lastQuotation == null){
-				new QuotationException("Quotation of the previous day or of day is not available!");
-			}
+		try {
+			LocalDate lastDate = dateQuotation.getDayOfWeek().getValue() == 7L ? dateQuotation.plusDays(-2)
+					: dateQuotation.plusDays(-1);
+
+			lastQuotation = CotacaoPredicate.filterCotacao(cotacoes, CotacaoPredicate.containsDate(lastDate))
+					.stream().filter(CotacaoPredicate.containsCurrency(currencyQuotationEntity.getTo())).findFirst().get();
+
+		} catch (NoSuchElementException e) {
+			new QuotationException("Quotation of the previous day or of day is not available!");
 		}
-		
-		
-		return null;
-	}
 
-	private CotacaoEntity getLastQuatation(List<CotacaoEntity> cotacoes2, LocalDate dateQuotation) {
-		// TODO Auto-generated method stub
-		return null;
+		return lastQuotation;
 	}
-
 }
--- src/br/com/currencyQuotation/domain/predicate/CotacaoPredicate.java
+++ src/br/com/currencyQuotation/domain/predicate/CotacaoPredicate.java
@@ -1,6 +1,7 @@
 package br.com.currencyQuotation.domain.predicate;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
@@ -12,12 +13,16 @@ public class CotacaoPredicate {
 	public static Predicate<CotacaoEntity> containsDate(LocalDate date) {
 		return c -> c.getDataCotacao().equals(date);
 	}
-	
+
 	public static Predicate<CotacaoEntity> containsCurrency(String currency) {
 		return c -> c.getMoeda().equals(currency);
 	}
 
-	public List<CotacaoEntity> filterCotacao(List<CotacaoEntity> cotacoes, Predicate<CotacaoEntity> predicate) {
-		return cotacoes.stream().filter(predicate).collect(Collectors.<CotacaoEntity> toList());
+	public static List<CotacaoEntity> filterCotacao(List<CotacaoEntity> cotacoes, Predicate<CotacaoEntity> predicate) {
+		try {
+			return cotacoes.stream().filter(predicate).collect(Collectors.<CotacaoEntity> toList());
+		} catch (Exception e) {
+			return new ArrayList<CotacaoEntity>();
+		}
 	}
 }
